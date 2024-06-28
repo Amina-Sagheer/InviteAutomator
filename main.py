1276f42adc03bbf88c1b0aadfba6fe31cc34d651
Amina-Sagheer <sagheeramina32@gmail.com>
2024-06-28 12:30:51 -0600

    Add complete main code

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+file_1 = open("./Input/Names/invited_names.txt", "r")
+names = file_1.readlines()
+for name in names:
+    name = name.strip()
+    file_2 = open("./Input/Letters/starting_letter.txt", "r")
+    letter_content = file_2.read()
+    modified_letter = letter_content.replace("[name]", name)
+    file_3 = open(f"./Output/ReadyToSend/{name}.txt", "w")
+    final_letter = file_3.write(modified_letter)
+    file_3.close()
+    file_2.close()
 
-
+file_1.close()
 
 
